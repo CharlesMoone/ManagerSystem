137729f37e8f0ebc5968955e0b102899ee04498d
CharlesMoone <meng5994197@gmail.com>
2016-06-25 11:22:21 +0800

    ［修复］更新结构
    1、新增一个类tipClass类，修改为全局类；
    2、取消使用require.js进行强制性先后加载；
    3、新增了addWarehouse.js进行区别与base.js

---
+++ js/tipClass.js
@@ -0,0 +1,54 @@
+/**
+ * Created by apple on 16/6/25.
+ */
+/**
+ * Main类,tip为提示文字对象
+ * @param tip
+ * @type {Object}
+ * @constructor Main
+ */
+var TipClass = function (tip) {
+    this.constructor = Main;
+    this.tip = tip || {};
+    /**
+     * 用来存储需要提示的目标及内容
+     * @type {{}}
+     * @public
+     */
+    this._tip = {};
+    this.input = $('input');
+    this._input = null;
+    var that = this;
+    (function () {
+        for (var i = 0; i < that.input.length; i ++) {
+            that._tip[that.input[i].name] = that.input[i].nextElementSibling.innerHTML;
+        }
+    })();
+};
+TipClass.prototype = {
+    getInput: function () {
+        var finish = true;
+        this._input = [];
+        for (var i = 0; i < this.input.length; i ++) {
+            if (this.input[i].dataset.checked == 'checked' && !this.input[i].value) {
+                this._input.push(this.input[i]);
+                finish = false;
+            }
+        }
+        return finish;
+    },
+    setTip: function () {
+        while (this._input.length) {
+            var input = this._input.pop();
+            input.nextElementSibling.innerHTML = this.tip[input.name] || ('不可为空');
+            input.nextElementSibling.style.display = 'inline-block';
+            input.className = "warning";
+        }
+    },
+    check: function () {
+        /**
+         * 做检查,如果发现某项的错误放出提示
+         */
+        return true;
+    }
+};
