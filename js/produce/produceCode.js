5c0ba87e3c37a71e407169a5ae25b995e7916977
CharlesMoone <meng5994197@gmail.com>
2016-07-08 14:02:51 +0800

    ［更新］小修复
    1、新增了全选效果；

--- js/produce/produceCode.js
+++ js/produce/produceCode.js
@@ -2,5 +2,8 @@
  * Created by apple on 16/7/2.
  */
 function pageLoad() {
-    
+    $("#selectAll").click(function (e) {
+        var checkBox = $('input[type=checkbox]');
+        $(this).prop('checked') == true ? checkBox.prop('checked', true) : checkBox.prop('checked', false);
+    });
 }
--- js/product/product.js
+++ js/product/product.js
@@ -2,5 +2,8 @@
  * Created by apple on 16/6/25.
  */
 function pageLoad() {
-
+    $("#selectAll").click(function (e) {
+        var checkBox = $('input[type=checkbox]');
+        $(this).prop('checked') == true ? checkBox.prop('checked', true) : checkBox.prop('checked', false);
+    });
 }
